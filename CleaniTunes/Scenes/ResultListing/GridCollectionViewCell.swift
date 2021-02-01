9d63f8359bce9b1778185791a25fcd0d1e72ed91
hager gamal <hager.gamal@linkdev.com>
2021-02-01 03:18:45 +0200

    add result listing Scene

---
+++ CleaniTunes/Scenes/ResultListing/GridCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  GridCollectionViewCell.swift
+//  CleaniTunes
+//
+//  Created by hager gamal on 1/30/21.
+//
+
+import UIKit
+
+class GridCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var subTitleLabel: UILabel!
+}
---
+++ CleaniTunes/Scenes/ResultListing/HeaderCollectionReusableView.swift
@@ -0,0 +1,13 @@
+//
+//  HeaderCollectionReusableView.swift
+//  CleaniTunes
+//
+//  Created by hager gamal on 1/30/21.
+//
+
+import UIKit
+
+class HeaderCollectionReusableView: UICollectionReusableView {
+        
+    @IBOutlet weak var genreLabel: UILabel!
+}
