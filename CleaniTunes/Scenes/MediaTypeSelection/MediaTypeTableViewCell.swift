480a791ab51e76677dc1d577a52b2a41ed2925d9
hager gamal <hager.gamal@linkdev.com>
2021-02-01 02:07:46 +0200

    add media type selection scene

---
+++ CleaniTunes/Scenes/MediaTypeSelection/MediaTypeTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  MediaTypeTableViewCell.swift
+//  CleaniTunes
+//
+//  Created by hager gamal on 1/29/21.
+//
+
+import UIKit
+
+class MediaTypeTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var labelTitle: UILabel!
+    @IBOutlet weak var imageSelect: UIImageView!
+    
+}
